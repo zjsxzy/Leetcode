d20cfee8c4913839ba127bfb7bd3f46480a9fe14
zjsxzy <zjsxzy@gmail.com>
2014-02-12 16:28:39 +0800

    Rotate Image

---
+++ Rotate Image/main.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+/*
+ * Description:
+ * You are given an n x n 2D matrix representing an image.
+ * Rotate the image by 90 degrees (clockwise).
+ *
+ * Solution:
+ * Brute force
+ */
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j < (n + 1) / 2; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[n - 1 - j][i];
+				matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+				matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+				matrix[j][n - 1 - i] = temp;
+			}
+		}
+    }
+};
+int main() {
+}
+
