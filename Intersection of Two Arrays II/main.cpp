3389a89e70f980117b2ef18a423a063e5ae9adf4
zjsxzy <zjsxzy@gmail.com>
2016-06-02 13:16:26 +0800

    update

--- Intersection of Two Arrays II/main.cpp
+++ Intersection of Two Arrays II/main.cpp
@@ -21,6 +21,28 @@ using namespace std;
 #define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
 typedef long long LL;
 
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        map<int, int> mp;
+        vector<int> res;
+        for (int i = 0; i < nums1.size(); i++) {
+            if (mp.find(nums1[i]) == mp.end()) {
+                mp.insert(pair<int, int>(nums1[i], 1));
+            } else {
+                mp[nums1[i]]++;
+            }
+        }
+        for (int i = 0; i < nums2.size(); i++) {
+            if (mp.find(nums2[i]) != mp.end()) {
+                if (mp[nums2[i]] > 0) {
+                    res.push_back(nums2[i]);
+                    mp[nums2[i]]--;
+                }
+            }
+        }
+        return res;
+    }
+};
 int main() {
 }
-
