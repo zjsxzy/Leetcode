66e0284e2645eb468b0fdfbe57a77f1f2e5db195
zjsxzy <zjsxzy@gmail.com>
2020-11-08 13:12:40 +0800

    add D

--- Weekly Contest 214/D/main.cpp
+++ Weekly Contest 214/D/main.cpp
@@ -23,6 +23,58 @@ using namespace std;
 #define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
 typedef long long LL;
 
+const int maxn = 100005;
+struct BinaryIndexTree {
+	int N;
+	int C[maxn];
+
+	void init(int N) {
+		this->N = N;
+		memset(C, 0, sizeof(C));
+	}
+
+	int lowbit(int x) {
+		return x & (-x);
+	}
+
+	void add(int x, int num) {
+		for (int i = x; i <= N; i += lowbit(i))
+			C[i] += num;
+	}
+
+	int sum(int x) {
+		int ret = 0;
+		for (int i = x; i > 0; i -= lowbit(i))
+			ret += C[i];
+		return ret;
+	}
+}bit;
+const int MOD = 1000000007;
+class Solution {
+public:
+    int createSortedArray(vector<int>& instructions) {
+        int n = instructions.size();
+        map<int, int> cnt;
+        int N = 100002;
+        bit.init(N);
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            int x = instructions[i];
+            int s = bit.sum(x);
+            int t = i;
+            if (cnt.find(x) != cnt.end()) {
+                s -= cnt[x];
+                t -= cnt[x];
+            }
+            t -= s;
+            res = (res + min(s, t)) % MOD;
+            cnt[x]++;
+            bit.add(x, 1);
+        }
+        return res;
+    }
+};
+
 int main() {
 }
 
