e16a4c18b4f94765b34126d8556430cc9a459cee
zjsxzy <zjsxzy@gmail.com>
2020-11-12 23:19:32 +0800

    wiggle subsequence

---
+++ Wiggle Subsequence/main.cpp
@@ -0,0 +1,50 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 2) return n;
+        vector<int> up(n), down(n);
+        up[0] = down[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = down[i - 1] + 1;
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                down[i] = up[i - 1] + 1;
+                up[i] = up[i - 1];
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return max(up[n - 1], down[n - 1]);
+    }
+};
+int main() {
+}
+
