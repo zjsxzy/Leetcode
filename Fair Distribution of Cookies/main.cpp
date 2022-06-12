43c1e308a74e2fd74367041acca9cd2c97e23ee6
zjsxzy <zjsxzy@gmail.com>
2022-06-12 11:29:08 +0800

    upd

--- Fair Distribution of Cookies/main.cpp
+++ Fair Distribution of Cookies/main.cpp
@@ -4,36 +4,30 @@ typedef long long LL;
 
 class Solution {
 public:
-    vector<vector<int>> has;
-    vector<int> c;
-    int n, k, res;
     int distributeCookies(vector<int>& cookies, int k) {
-        n = cookies.size();
-        has.assign(n, {});
-        this->k = k;
-        c = cookies;
-        res = INT_MAX;
+        int n = cookies.size();
+        vector<vector<int>> has(n);
+        int res = INT_MAX;
+        function<void(int)> dfs = [&](int p) {
+            if (p == n) {
+                int mx = 0;
+                for (int i = 0; i < k; i++) {
+                    if (has[i].size() == 0) return;
+                    int sum = 0;
+                    for (auto x: has[i]) sum += cookies[x];
+                    mx = max(mx, sum);
+                }
+                res = min(res, mx);
+                return;
+            }
+            for (int i = 0; i < k; i++) {
+                has[i].push_back(p);
+                dfs(p + 1);
+                has[i].pop_back();
+            }
+        };
         dfs(0);
         return res;
-    }
-
-    void dfs(int p) {
-        if (p == n) {
-            int mx = 0;
-            for (int i = 0; i < k; i++) {
-                if (has[i].size() == 0) return;
-                int sum = 0;
-                for (auto x: has[i]) sum += c[x];
-                mx = max(mx, sum);
-            }
-            res = min(res, mx);
-            return;
-        }
-        for (int i = 0; i < k; i++) {
-            has[i].push_back(p);
-            dfs(p + 1);
-            has[i].pop_back();
-        }
     }
 };
 
