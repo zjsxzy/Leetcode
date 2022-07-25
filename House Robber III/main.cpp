768f35b0a1a6f9a47652f2250f955ee8e19305d3
Zhang Yang <zjsxzy@gmail.com>
2022-07-25 10:34:56 +0800

    house robber iii

---
+++ House Robber III/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*, int> f, g;
+    int rob(TreeNode* root) {
+        dfs(root);
+        return max(f[root], g[root]);
+    }
+    void dfs(TreeNode* node) {
+        if (!node) return;
+        dfs(node->left);
+        dfs(node->right);
+        f[node] = node->val + g[node->left] + g[node->right];
+        g[node] = max(f[node->left], g[node->left]) + max(f[node->right], g[node->right]);
+    }
+};
+
+void solve() {
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+   (void)!freopen("in.txt", "r", stdin);
+#endif
+   int ts = 1;
+   // cin >> ts;
+   for (int t = 1; t <= ts; t++) {
+       solve();
+   }
+   return 0;
+}
+
