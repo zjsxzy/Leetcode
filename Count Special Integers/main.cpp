fbac27e30d2b2316a6c02cfbcfd43a7c2d09983f
Zhang Yang <zjsxzy@gmail.com>
2022-08-15 16:29:37 +0800

    upd

--- Count Special Integers/main.cpp
+++ Count Special Integers/main.cpp
@@ -4,32 +4,34 @@ typedef long long LL;
 
 class Solution {
 public:
+    vector<int> digits;
+    vector<vector<int>> dp;
+
     int countSpecialNumbers(int n) {
-        vector<int> digits;
-        int x = n + 1;
+        digits.clear();
+        int x = n;
         while (x) {
             digits.push_back(x % 10);
             x /= 10;
         }
         reverse(digits.begin(), digits.end());
-        int res = 0, sz = digits.size();
-        for (int i = 1; i < sz; ++i) {
-            res += 9 * A(9, i - 1);
-        }
 
-        unordered_set<int> visited;
-        for (int i = 0; i < sz; i++) {
-            for (int j = i > 0 ? 0 : 1; j < digits[i]; j++) {
-                if (visited.count(j)) continue;
-                res += A(9 - i, sz - i - 1);
-            }
-            if (visited.count(digits[i])) break;
-            visited.insert(digits[i]);
-        }
-        return res;
+        dp.assign((int)digits.size(), vector<int>(1 << 10, -1));
+        return dfs(0, 0, true, false);
     }
-    int A(int m, int n) {
-        return n == 0 ? 1 : A(m, n - 1) * (m - n + 1);
+
+    int dfs(int i, int mask, bool is_limit, bool is_num) {
+        if (i == (int)digits.size()) return is_num;
+        if (!is_limit && is_num && dp[i][mask] >= 0) return dp[i][mask];
+        int res = 0;
+        if (!is_num) res = dfs(i + 1, mask, false, false); // 跳过当前数位
+        for (int d = 1 - is_num, up = is_limit ? digits[i] : 9; d <= up; d++) {
+            if ((mask >> d & 1) == 0) {
+                res += dfs(i + 1, mask | (1 << d), is_limit && d == up, true);
+            }
+        }
+        if (!is_limit &&is_num) dp[i][mask] = res;
+        return res;
     }
 };
 
