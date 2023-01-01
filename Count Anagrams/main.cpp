f79440058257e180f0355d73a4b1cf137e947cd6
zhangyang <zjsxzy@gmail.com>
2023-01-01 19:18:18 +0800

    count anagrams

---
+++ Count Anagrams/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+const int MOD = 1e9 + 7;
+template <int mod>
+struct Mod_Int {
+    int x;
+    Mod_Int() : x(0) {}
+    Mod_Int(long long y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+    static int get_mod() { return mod; }
+    Mod_Int &operator+=(const Mod_Int &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator-=(const Mod_Int &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+    Mod_Int &operator*=(const Mod_Int &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+    Mod_Int &operator/=(const Mod_Int &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+    Mod_Int &operator++() { return *this += Mod_Int(1); }
+    Mod_Int operator++(int) {
+        Mod_Int tmp = *this;
+        ++*this;
+        return tmp;
+    }
+    Mod_Int &operator--() { return *this -= Mod_Int(1); }
+    Mod_Int operator--(int) {
+        Mod_Int tmp = *this;
+        --*this;
+        return tmp;
+    }
+    Mod_Int operator-() const { return Mod_Int(-x); }
+    Mod_Int operator+(const Mod_Int &p) const { return Mod_Int(*this) += p; }
+    Mod_Int operator-(const Mod_Int &p) const { return Mod_Int(*this) -= p; }
+    Mod_Int operator*(const Mod_Int &p) const { return Mod_Int(*this) *= p; }
+    Mod_Int operator/(const Mod_Int &p) const { return Mod_Int(*this) /= p; }
+    bool operator==(const Mod_Int &p) const { return x == p.x; }
+    bool operator!=(const Mod_Int &p) const { return x != p.x; }
+    Mod_Int inverse() const {
+        assert(*this != Mod_Int(0));
+        return pow(mod - 2);
+    }
+    Mod_Int pow(long long k) const {
+        Mod_Int now = *this, ret = 1;
+        for (; k > 0; k >>= 1, now *= now) {
+            if (k & 1) ret *= now;
+        }
+        return ret;
+    }
+    friend ostream &operator<<(ostream &os, const Mod_Int &p) { return os << p.x; }
+    friend istream &operator>>(istream &is, Mod_Int &p) {
+        long long a;
+        is >> a;
+        p = Mod_Int<mod>(a);
+        return is;
+    }
+};
+using mint = Mod_Int<MOD>;
+
+class Solution {
+public:
+    int countAnagrams(string s) {
+        s += ' ';
+        int n = s.size();
+        mint res = 1;
+        vector<int> cnt(26);
+        for (int i = 0; i < n; i++) {
+            if (s[i] == ' ') {
+                mint t = calc(cnt);
+                res *= t;
+                cnt = vector<int>(26, 0);
+            } else {
+                cnt[s[i] - 'a']++;
+            }
+        }
+        return res.x;
+    }
+
+    mint permutation(int n) {
+        mint res = 1;
+        for (int i = 1; i <= n; i++) res *= i;
+        return res;
+    }
+
+    mint calc(vector<int>& cnt) {
+        int n = 0;
+        for (auto x: cnt) n += x;
+        mint res = permutation(n);
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] == 0) continue;
+            res /= permutation(cnt[i]);
+        }
+        return res;
+    }
+};
+
+void solve() {
+}
+
+int main() {
+    int ts = 1;
+    // cin >> ts;
+    for (int t = 1; t <= ts; t++) {
+        solve();
+    }
+    return 0;
+}
+
